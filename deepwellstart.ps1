6fe27aa6b0e49c5dd3a6f8148b4f44768f4343c4
torsteinotterlei <torstein@otterlei.no>
2020-07-06 07:43:09 +0200

    Fixed script for running docker container

--- deepwellstart.ps1
+++ deepwellstart.ps1
@@ -15,16 +15,16 @@ if ($build -or $b) {
     docker build -t deepwell-app .
 }
 elseif ($run -or $r) {
-    docker rm -f dwrunning ; if ($?){ docker run -it --mount type=bind,source="$(pwd)",target=/app -p 8080:8080 --name dwrunning deepwell-app }
+    docker rm -f dwrunning ; docker run -it --mount type=bind,source="$(pwd)",target=/app -p 8080:8080 --name dwrunning deepwell-app
 }
 elseif ($show -or $s -or $rs) {
-    docker rm -f dwrunning ; if ($?){docker run -dit --mount type=bind,source="$(pwd)",target=/app -p 8080:8080 --name dwrunning deepwell-app}; if ($?){ Start-Sleep -s 2; Start "http://localhost:8080/"; docker logs -f dwrunning }
+    docker rm -f dwrunning ; docker run -dit --mount type=bind,source="$(pwd)",target=/app -p 8080:8080 --name dwrunning deepwell-app ; if ($?){ Start-Sleep -s 2; Start "http://localhost:8080/"; docker logs -f dwrunning }
 }
 elseif ($br) {
-    docker build -t deepwell-app .; if ($?){docker rm -f dwrunning} ; if ($?){docker run -it --mount type=bind,source="$(pwd)",target=/app -p 8080:8080 --name dwrunning deepwell-app }
+    docker build -t deepwell-app . ; if ($?){docker rm -f dwrunning} ; docker run -it --mount type=bind,source="$(pwd)",target=/app -p 8080:8080 --name dwrunning deepwell-app
 }
 elseif ($brs -or $bs) {
-    docker build -t deepwell-app . ; if ($?){docker rm -f dwrunning} ; if ($?){docker run -dit --mount type=bind,source="$(pwd)",target=/app -p 8080:8080 --name dwrunning deepwell-app}; if ($?){ Start-Sleep -s 2; Start "http://localhost:8080/"; docker logs -f dwrunning }
+    docker build -t deepwell-app . ; if ($?){docker rm -f dwrunning} ; docker run -dit --mount type=bind,source="$(pwd)",target=/app -p 8080:8080 --name dwrunning deepwell-app; if ($?){ Start-Sleep -s 2; Start "http://localhost:8080/"; docker logs -f dwrunning }
 }
 else{
     write-output "No accepted flags detected. Choose from -b,-r,-rs,-br or -brs. b=build, r=run, s=show like: .\deepwellstart.ps1 -br"
