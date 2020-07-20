1bcb285be107c9ce9bf181eb219914635439ccf7
Matias HM <myrestrand96@hotmail.com>
2020-07-20 09:42:06 +0200

    Add latest version of spherical env

--- env/gym-dw/gym_dw/envs/DeepWellEnvSpher.py
+++ env/gym-dw/gym_dw/envs/DeepWellEnvSpher.py
@@ -36,44 +36,47 @@ class DeepWellEnvSpher(gym.Env):
         self.min_radius_hazard = 100
         self.max_radius_hazard = 100    
 
-        self.state = self.init_states() #[xdist1, ydist1, zdist1, xdist2, ydist2, zdist2 xd, yd, zd, x_hz_dist, y_hz_dist, z_hz_dist]
+        self.state = self.init_states()
         
         #Set action and observation space
         incr = ANGACC_INCREMENT
         self.actions_dict = {
             0:[-incr, -incr],
             1:[-incr, 0],
-            2:[-incr, incr],          # Actions: [0] = vertical acceleration,
+            2:[-incr, incr],       # Actions: [0] = vertical acceleration,
             3:[0, -incr],          # [1] = horizontal acceleration 
-            4:[0, 0],           # -1:Decreaase, 0:Maintain, 1:Increase
+            4:[0, 0],              # -1:Decreaase, 0:Maintain, 1:Increase
             5:[0, incr],
             6:[incr, -incr],
             7:[incr, 0],
             8:[incr, incr],}        
         self.action_space = spaces.Discrete(9)
 
-        state_high = np.array([self.xmax, self.ymax, self.zmax,   # xdist1, ydist1, zdist1,
-                                self.xmax, self.ymax, self.zmax,  # xdist2, ydist2, zdist2,
-                                self.xmax, self.ymax, self.zmax,  # xdist_hazard, ydist_hazard, zdist_hazard,
-                                np.pi, 2*np.pi,                   # vertical_ang, horizontal_ang,
-                                MAX_ANGVEL, MAX_ANGVEL,           # vertical_ang_vel, horizontal_ang_vel,
-                                MAX_ANGACC, MAX_ANGACC])          # vertical_ang_acc, horizontal_ang_acc
+
+        state_high = np.array([MAX_ANGACC, MAX_ANGACC,        # vertical_ang_acc, horizontal_ang_acc,
+                            MAX_ANGVEL, MAX_ANGVEL,            # vertical_ang_vel, horizontal_ang_vel,
+                            np.pi, np.pi #(self.xmax**2+self.ymax**2+self.zmax**2)**(1./2.),       #rel_vertical_target_ang1, rel_horizontal_target_ang1, target_dist1
+                           #np.pi, np.pi, (self.xmax**2+self.ymax**2+self.zmax**2)**(1./2.),       #rel_vertical_target_ang2, rel_horizontal_target_ang2, target_dist2
+                           #np.pi, np.pi, (self.xmax**2+self.ymax**2+self.zmax**2)**(1./2.)       #rel_vertical_hazard_ang, rel_horizontal_hazard_ang, hazard_dist
+                        ])
+    
+
         state_low = -state_high.copy()
-        state_low[9], state_low[10] = 0, 0
+        #state_low[6], state_low[9], state_low[12] = 0, 0, 0 
         self.observation_space = spaces.Box(low=state_low, high=state_high, dtype=np.float64)
 
 
     def init_states(self):
-        #Set starting drill position and velocity
+        #Set starting drill position
         self.dist_traveled = 0
         self.target_hits = 0
-        self.x = random.randint(200, 600)
+        self.x = random.randint(0, 600)
         self.y = self.ymax/2
         self.z = 0 
 
         # Spherical coordinates
-        self.horizontal_ang = 0  # random.uniform(np.pi/10, np.pi/2-np.pi/10)  # change later?
-        self.vertical_ang = 0    # starting vertically down
+        self.horizontal_ang = random.uniform(0,2*np.pi)  # random.uniform(np.pi/10, np.pi/2-np.pi/10)  # change later?
+        self.vertical_ang = random.uniform(0,5*np.pi/180)
 
         self.horizontal_angVel = 0
         self.vertical_angVel = 0
@@ -81,44 +84,30 @@ class DeepWellEnvSpher(gym.Env):
         self.horizontal_angAcc = 0
         self.vertical_angAcc = 0
 
+        #Set starting drill velocity
+        self.xd = STEP_LENGTH * np.sin(self.vertical_ang) * np.cos(self.horizontal_ang)
+        self.yd = STEP_LENGTH * np.sin(self.vertical_ang) * np.sin(self.horizontal_ang)
+        self.zd = STEP_LENGTH * np.cos(self.vertical_ang)
 
-        #Initialize target(s)
+        #Initialize targets and hazards
         self.targets = self.init_targets()
         self.hazards = self.init_hazards()
-        self.xdist1 = self.x - self.targets[0]['pos'][0]
-        self.ydist1 = self.y - self.targets[0]['pos'][1]
-        self.zdist1 = self.z - self.targets[0]['pos'][2]
-
-        if self.numtargets > 1:
-            self.xdist2 = self.x - self.targets[1]['pos'][0]
-            self.ydist2 = self.y - self.targets[1]['pos'][1]
-            self.zdist2 = self.y - self.targets[1]['pos'][2]
-
-
+        self.calc_dist_to_target()
+        self.calc_target_ang()
+        self.calc_rel_target_ang()
+        
         #Set distances to closest hazard
         if self.numhazards > 0:
-            diff = [(np.array(hazard['pos']) - [self.x, self.y, self.z]) for hazard in self.hazards]
-            diffnorms = [np.linalg.norm([element[0], element[1], element[2]]) for element in diff]
-            closest_hz = np.argmin(diffnorms)
-            self.xdist_hazard = diff[closest_hz][0]
-            self.ydist_hazard = diff[closest_hz][1]
-            self.zdist_hazard = diff[closest_hz][2]
+            _ = self.calc_hazard_distances()
+            self.hazard_dist = np.linalg.norm([self.xdist_hazard, self.ydist_hazard, self.zdist_hazard])
         else:
             self.xdist_hazard = -self.xmax + 2*random.randint(0,1)*self.xmax
             self.ydist_hazard = -self.ymax + 2*random.randint(0,1)*self.ymax
             self.zdist_hazard = -self.zmax + 2*random.randint(0,1)*self.zmax
 
-        #Calculate minimum and maximum total distance
-        self.max_dist = []
-        self.min_tot_dist = 0
-        prev_p = np.array([self.x,self.y, self.z])
-        for i in range(self.numtargets):
-            self.min_tot_dist += np.linalg.norm([self.targets[i]['pos'][0]
-            -prev_p[0],self.targets[i]['pos'][1]-prev_p[1], self.targets[i]['pos'][2]-prev_p[2]])
-            prev_p = np.array(self.targets[i]['pos'])
-            self.max_dist.append(self.rel_max_dist*self.min_tot_dist)
-        
-        self.max_tot_dist = self.rel_max_dist*self.min_tot_dist
+        self.calc_hazard_ang()
+        self.calc_rel_hazard_ang()  
+        self.max_tot_dist = self.calc_max_tot_dist()
 
         state = self.get_state()
         return state
@@ -126,23 +115,25 @@ class DeepWellEnvSpher(gym.Env):
 
     def get_state(self):
         state = np.array([
-            self.xdist1, self.ydist1, self.zdist1,
-            self.xdist2, self.ydist2, self.zdist2,
-            self.xdist_hazard, self.ydist_hazard, self.zdist_hazard,
-            self.vertical_ang, self.horizontal_ang,
+            self.vertical_angAcc, self.horizontal_angAcc,
             self.vertical_angVel, self.horizontal_angVel,
-            self.vertical_angAcc, self.horizontal_angAcc])
+            self.rel_vertical_target_ang1, self.rel_horizontal_target_ang1#, self.target_dist1,
+            #self.rel_vertical_target_ang2, self.rel_horizontal_target_ang2, self.target_dist2,
+            #self.rel_vertical_hazard_ang, self.rel_horizontal_hazard_ang, self.hazard_dist
+            ])
         return state
 
                
     def step(self, action):
 
-        self.old_dist = np.linalg.norm([self.xdist1, self.ydist1, self.zdist1]) #Distance to next target
-        
+        self.old_dist = np.linalg.norm([self.xdist1, self.ydist1, self.zdist1]) #Distance to next target 
         self.update_pos(action)
-        self.calc_dist_to_target()
+        self.calc_dist_to_target()        
         reward, done = self.get_reward()
-
+        self.calc_target_ang()
+        self.calc_rel_target_ang()
+        self.calc_hazard_ang()
+        self.calc_rel_hazard_ang()
         state = self.get_state()
         info = self.get_info(done)
 
@@ -150,13 +141,13 @@ class DeepWellEnvSpher(gym.Env):
 
 
     def update_pos(self, action):
+        acc = self.actions_dict[action]
         
-        # update angular acceleration
-        if abs(self.vertical_angAcc + self.actions_dict[action][0]) <= MAX_ANGACC:
-            self.vertical_angAcc += self.actions_dict[action][0]
+        if abs(self.vertical_angAcc + acc[0]) <= MAX_ANGACC:
+            self.vertical_angAcc += acc[0]
         
-        if abs(self.horizontal_angAcc + self.actions_dict[action][1]) <= MAX_ANGACC:
-            self.horizontal_angAcc += self.actions_dict[action][1]
+        if abs(self.horizontal_angAcc + acc[1]) <= MAX_ANGACC:
+            self.horizontal_angAcc += acc[1]
         
         # Update angular velocity
         if abs(self.vertical_angVel + self.vertical_angAcc) <= MAX_ANGVEL:
@@ -166,13 +157,23 @@ class DeepWellEnvSpher(gym.Env):
             self.horizontal_angVel += self.horizontal_angAcc
         
         # Update angle
-        self.vertical_ang = (self.vertical_ang + self.vertical_angVel) % (2*np.pi)
+        self.vertical_ang = (self.vertical_ang + self.vertical_angVel)
+        if self.vertical_ang < 0:
+            self.vertical_ang = 0
+            self.vertical_angVel = 0
+        if self.vertical_ang > np.pi:
+            self.vertical_ang = np.pi
+            self.vertical_angVel = 0
+
         self.horizontal_ang = (self.horizontal_ang + self.horizontal_angVel) % (2*np.pi)
 
-        # update position
-        self.x += STEP_LENGTH * np.sin(self.vertical_ang) * np.cos(self.horizontal_ang)
-        self.y += STEP_LENGTH * np.sin(self.vertical_ang) * np.sin(self.horizontal_ang)
-        self.z += STEP_LENGTH * np.cos(self.vertical_ang)
+        # update position and velocity
+        self.xd = STEP_LENGTH * np.sin(self.vertical_ang) * np.cos(self.horizontal_ang)
+        self.yd = STEP_LENGTH * np.sin(self.vertical_ang) * np.sin(self.horizontal_ang)
+        self.zd = STEP_LENGTH * np.cos(self.vertical_ang)
+        self.x += self.xd
+        self.y += self.yd
+        self.z += self.zd
 
     def calc_dist_to_target(self):
         #Calculate and update distance to target(s)
@@ -187,44 +188,71 @@ class DeepWellEnvSpher(gym.Env):
             self.xdist2 = self.targets[self.target_hits+1]['pos'][0] - self.x  #x-axis distance to second next target
             self.ydist2 = self.targets[self.target_hits+1]['pos'][1] - self.y  #y-axis distance to second next target
             self.zdist2 = self.targets[self.target_hits+1]['pos'][2] - self.z  #z-axis distance to second next target
+        self.target_dist1 = np.linalg.norm([self.xdist1, self.ydist1, self.zdist1])
+        self.target_dist2 = np.linalg.norm([self.xdist2, self.ydist2, self.zdist2])
 
+    def calc_target_ang(self):
+        self.vertical_target_ang1 = np.arctan2(np.sqrt(self.xdist1**2 + self.ydist1**2),self.zdist1)
+        self.horizontal_target_ang1 = np.arctan2(self.ydist1,self.xdist1)
+        self.vertical_target_ang2 = np.arctan2(np.sqrt(self.xdist2**2 + self.ydist2**2),self.zdist2)
+        self.horizontal_target_ang2 = np.arctan2(self.ydist2,self.xdist2)
 
+    def calc_rel_target_ang(self):
+        self.rel_vertical_target_ang1 = calc_angle_diff(self.vertical_ang, self.vertical_target_ang1)
+        self.rel_horizontal_target_ang1 = calc_angle_diff(self.horizontal_ang, self.horizontal_target_ang1)
+        self.rel_vertical_target_ang2 = calc_angle_diff(self.vertical_ang, self.vertical_target_ang2)
+        self.rel_horizontal_target_ang2 = calc_angle_diff(self.horizontal_ang, self.horizontal_target_ang2)
+    
+    def calc_hazard_ang(self):
+        self.vertical_hazard_ang = np.arctan2(np.sqrt(self.xdist_hazard**2 + self.ydist_hazard**2),self.zdist_hazard)
+        self.horizontal_hazard_ang = np.arctan2(self.ydist_hazard,self.xdist_hazard)
+
+    def calc_rel_hazard_ang(self):
+        self.rel_vertical_hazard_ang = calc_angle_diff(self.vertical_ang, self.vertical_hazard_ang)
+        self.rel_horizontal_hazard_ang = calc_angle_diff(self.horizontal_ang, self.horizontal_hazard_ang)
+
+    def calc_hazard_distances(self):
+        diff = [(np.array(hazard['pos'])-[self.x,self.y,self.z]) for hazard in self.hazards]
+        diffnorms = [np.linalg.norm([element[0], element[1], element[2]]) for element in diff]
+        closest_hz = np.argmin(diffnorms)
+        self.xdist_hazard = diff[closest_hz][0]
+        self.ydist_hazard = diff[closest_hz][1]
+        self.zdist_hazard = diff[closest_hz][2]
+        return closest_hz
+
+    def calc_max_tot_dist(self):
+        self.max_dist = []
+        self.min_tot_dist = 0
+        prev_p = np.array([self.x,self.y, self.z])
+        for i in range(self.numtargets):
+            self.min_tot_dist += np.linalg.norm([self.targets[i]['pos'][0]
+            -prev_p[0],self.targets[i]['pos'][1]-prev_p[1], self.targets[i]['pos'][2]-prev_p[2]])
+            prev_p = np.array(self.targets[i]['pos'])
+            self.max_dist.append(self.rel_max_dist*self.min_tot_dist)      
+        max_tot_dist = self.rel_max_dist*self.min_tot_dist
+        return max_tot_dist
+        
     def get_reward(self):
         done = False
         #Check new target distance (reward)
         dist_new = np.linalg.norm([self.xdist1,self.ydist1, self.zdist1])  
         dist_diff = dist_new - self.old_dist
-        reward = -dist_diff
+        reward = -3*dist_diff
 
-        if abs(self.vertical_angAcc) >= MAX_ANGACC:
-            reward -= 100
-        if abs(self.horizontal_angAcc) >= MAX_ANGACC:
-            reward -= 100
-
-        if abs(self.vertical_angVel) >= MAX_ANGVEL:
-            reward -= 100
-        if abs(self.horizontal_angVel) >= MAX_ANGVEL:
-            reward -= 100
-
-        if self.vertical_angVel == 0 and self.horizontal_angVel == 0:
-            reward +=100
-        
+        reward -= abs(self.rel_vertical_target_ang1) + abs(self.rel_horizontal_target_ang1)
+               
         if self.numhazards > 0:
-            diff = [(np.array(hazard['pos'])-[self.x,self.y,self.z]) for hazard in self.hazards]
-            diffnorms = [np.linalg.norm([element[0], element[1], element[2]]) for element in diff]
-            closest_hz = np.argmin(diffnorms)
-            dist_hazard = diffnorms[closest_hz]
-            
-            if dist_hazard < self.hazards[closest_hz]['radius']:
+            closest_hz = self.calc_hazard_distances()
+            self.hazard_dist = np.linalg.norm([self.xdist_hazard, self.ydist_hazard, self.zdist_hazard])
+
+            if self.hazard_dist < self.hazards[closest_hz]['radius']:
                 reward -= 2000
                 done = True
             
-            if dist_hazard < self.hazards[closest_hz]['radius']*2:
-                rel_safe_dist = (self.hazards[closest_hz]['radius']*2 - dist_hazard)/(self.hazards[closest_hz]['radius']) # 0 if dist_hazard = 2*radius_hazard, 1 if dist_hazard = radius_hazard
+            if self.hazard_dist < self.hazards[closest_hz]['radius']*2:
+                rel_safe_dist = (self.hazards[closest_hz]['radius']*2 - self.hazard_dist)/(self.hazards[closest_hz]['radius']) # 0 if dist_hazard = 2*radius_hazard, 1 if dist_hazard = radius_hazard
                 reward -= 50*rel_safe_dist**2
-            self.xdist_hazard = diff[closest_hz][0]
-            self.ydist_hazard = diff[closest_hz][1]
-            self.zdist_hazard = diff[closest_hz][2]
+
 
         #Check if outside grid (reward)
         if (self.x<self.xmin) or (self.y<self.ymin) or (self.z<self.zmin)or (self.x>self.xmax) or (self.y>self.ymax) or (self.z>self.zmax):
@@ -252,13 +280,12 @@ class DeepWellEnvSpher(gym.Env):
                     self.xdist2 = self.targets[self.target_hits+1]['pos'][0]-self.x  #x-axis distance to next target
                     self.ydist2 = self.targets[self.target_hits+1]['pos'][1]-self.y  #y-axis distance to next target
                     self.zdist2 = self.targets[self.target_hits+1]['pos'][2]-self.z  #z-axis distance to next target
-                else: 
-                    self.xdist2 = self.xdist1
+                else:
+                    self.xdist2 = self.xdist1   #Duplicate target if only one left
                     self.ydist2 = self.ydist1
                     self.zdist2 = self.zdist1
 
         return reward, done
-
 
     def get_info(self, done):
         #Info for plotting and printing in run-file
@@ -377,6 +404,14 @@ def plot_ball(x0, y0, z0, r, c, ax):
         z = z0 + r * np.outer(np.ones(np.size(u)), np.cos(v))
         # Plot the surface
         ax.plot_surface(x, y, z, color=c)
+
+def calc_angle_diff(ang1, ang2):
+    diff = ang2 - ang1
+    if diff < -np.pi:
+        diff = diff + 2*np.pi
+    if diff > np.pi:
+        diff = diff - 2*np.pi
+    return diff
 
 
 if __name__ == '__main__' :
