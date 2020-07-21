5778672387dcc9958bdf866fa64208edeec818aa
Jakob Torben <jakob_torben@hotmail.com>
2020-07-21 12:34:17 +0200

    Add different levels of parentclass

---
+++ env/gym-dw/gym_dw/envs/DeepWellEnvSpherLevels.py
@@ -0,0 +1,72 @@
+from gym_dw.envs.DeepWellEnvSpher import DeepWellEnvSpher
+
+
+class Level1(DeepWellEnvSpher):
+    
+    def __init__(self):
+        DeepWellEnvSpher.__init__(self)
+        self.deltaY_target = 0
+        self.maxdeltaZ_target = 0
+        self.numhazards = 0
+        self.min_radius = 100
+        self.max_radius = 100
+        self.reset()
+    
+
+class Level2(DeepWellEnvSpher):
+    
+    def __init__(self):
+        DeepWellEnvSpher.__init__(self)
+        self.deltaY_target = 0
+        self.maxdeltaZ_target = 100
+        self.numhazards = 0
+        self.min_radius = 100
+        self.max_radius = 100
+        self.reset()
+
+
+class Level3(DeepWellEnvSpher):
+    
+    def __init__(self):
+        DeepWellEnvSpher.__init__(self)
+        self.deltaY_target = 100
+        self.maxdeltaZ_target = 100
+        self.numhazards = 0
+        self.min_radius = 100
+        self.max_radius = 100
+        self.reset()
+
+
+class Level4(DeepWellEnvSpher):
+    
+    def __init__(self):
+        DeepWellEnvSpher.__init__(self)
+        self.deltaY_target = 200
+        self.maxdeltaZ_target = 500
+        self.numhazards = 0
+        self.min_radius = 50
+        self.max_radius = 50
+        self.reset()
+
+
+class Level5(DeepWellEnvSpher):
+    
+    def __init__(self):
+        DeepWellEnvSpher.__init__(self)
+        self.deltaY_target = 200
+        self.maxdeltaZ_target = 500
+        self.numhazards = 2
+        self.min_radius = 50
+        self.max_radius = 50
+        self.reset()
+
+
+
+if __name__ == '__main__' :
+    env = DeepWellEnvSpher()
+    env.reset()
+    for _ in range(10):
+        action = 0 #env.action_space.sample()
+        print(env.actions_dict[action])
+        print("Step: ", _ , " this is what the current state is:")
+        print(env.step(action))
