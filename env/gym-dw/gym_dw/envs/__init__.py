565635df1a31a6f3839c90004182aeef43e33303
Aadne S Djuve <aadnesd@stud.ntnu.no>
2020-07-30 15:05:30 +0200

    Fix name bug in init env file

--- env/gym-dw/gym_dw/envs/__init__.py
+++ env/gym-dw/gym_dw/envs/__init__.py
@@ -3,4 +3,4 @@ from gym_dw.envs.DeepWellEnv_v2 import DeepWellEnvV2
 from gym_dw.envs.DeepWellEnv3d import DeepWellEnv3D
 from gym_dw.envs.DeepWellEnvSpher import DeepWellEnvSpher
 from gym_dw.envs.DeepWellEnvSpherLevels import *
-from gym_dw.envs.DeepWellEnvSpherSmallObs import DeepWellEnvSpher
+from gym_dw.envs.DeepWellEnvSpherSmallObs import DeepWellEnvSpherSmallObs
